6114343f9a3029f7f5cc922da3938c14416a74df
Giacomo <giacomo.redjack@gmail.com>
2021-03-25 20:25:19 +0100

    modifica

--- src/main/java/com/giacomolorenzo/rossi/HelloTravisMaven.java
+++ src/main/java/com/giacomolorenzo/rossi/HelloTravisMaven.java
@@ -7,7 +7,7 @@ public class HelloTravisMaven {
 
 	public static void main(String[] args) {
 		Logger.getGlobal().log(Level.INFO, System.getProperty("java.runtime.version"));
-		Logger.getGlobal().log(Level.INFO, "Prova");
+		Logger.getGlobal().log(Level.INFO, "Prova1");
 	}
 
 }
